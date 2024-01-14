8c20fd5ebba1c132ff32ec43d03d70c51a771838
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-14 21:28:21 +0100

    feat: add user age

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@
 const userDistance = parseInt (prompt("Quanti km vuoi percorrere?"));
 console.log ('Distanza utente ' + userDistance + ' km');
 
+// chiedo l'età dell'utente
+const userAge = parseInt (prompt("Quanti anni hai?"));
+console.log ('Età utente ' + userAge + ' anni');
+
 
