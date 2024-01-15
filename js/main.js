8e4bb3e987103f16d0f89c0729fd38de2b244e57
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-15 17:09:10 +0100

    fix: if is Nan error

--- js/main.js
+++ js/main.js
@@ -14,24 +14,29 @@ console.log (ticketPrice + " Euro");
 document.getElementById ("ticket_result").innerText = (ticketPrice + " Euro")
 console.log ("Il prezzo è di " + ticketPrice + " Euro")
 
+
+if (!isNaN(userDistance) && !isNaN(userAge)) {
+    if(userAge < 18) {
+        // calcolo e applico sconto 20%
+        let discountPriceUnder = (ticketPrice - (ticketPrice * 0.2))
+        console.log ("Se minorenne il prezzo è di " + discountPriceUnder.toFixed(2) + " Euro")
+    
+        // stampo biglietto
+        document.getElementById ("ticket_result").innerText = (discountPriceUnder.toFixed(2) + " Euro")
+        console.log ("Il prezzo è di " + discountPriceUnder.toFixed(2) + " Euro")
+    
+        // se over 65
+    } else if(userAge > 65) {
+        // calcolo e applico sconto 40%
+        let discountPriceOver = (ticketPrice - (ticketPrice * 0.4))
+        console.log ("Se over 65 il prezzo è di " + discountPriceOver.toFixed(2) + " Euro")
+    
+        // stampo biglietto
+        document.getElementById ("ticket_result").innerText = (discountPriceOver.toFixed(2) + " Euro")
+        console.log ("Il prezzo è di " + discountPriceOver.toFixed(2) + " Euro")
+    }
+} else {
+    document.getElementById ("ticket_result").innerText = ("Dati non validi")
+}
 // se minorenne
-if(userAge < 18) {
-    // calcolo e applico sconto 20%
-    let discountPriceUnder = (ticketPrice - (ticketPrice * 0.2))
-    console.log ("Se minorenne il prezzo è di " + discountPriceUnder.toFixed(2) + " Euro")
 
-    // stampo biglietto
-    document.getElementById ("ticket_result").innerText = (discountPriceUnder.toFixed(2) + " Euro")
-    console.log ("Il prezzo è di " + discountPriceUnder.toFixed(2) + " Euro")
-}
-
-// se over 65
-if(userAge > 65) {
-    // calcolo e applico sconto 40%
-    let discountPriceOver = (ticketPrice - (ticketPrice * 0.4))
-    console.log ("Se over 65 il prezzo è di " + discountPriceOver.toFixed(2) + " Euro")
-
-    // stampo biglietto
-    document.getElementById ("ticket_result").innerText = (discountPriceOver.toFixed(2) + " Euro")
-    console.log ("Il prezzo è di " + discountPriceOver.toFixed(2) + " Euro")
-}
